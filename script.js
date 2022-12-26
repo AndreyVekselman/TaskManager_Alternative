d228dfe2bebd41858671f8616d37046244e40eb7
Andrey Vekselman <Andrey.Vekselman@gmail.com>
2022-12-26 20:57:33 +0200

    delete all not relevant code

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ let routes = {
   "/about": "about",
   "/createTask": "createTask",
 };
+
 function resolveRoute(routeURL) {
   try {
     return routes[routeURL]; //routes['/about'] == 'about'
@@ -64,92 +65,4 @@ window.addEventListener("load", router);
 window.addEventListener("hashchange", router);
 
 window.addEventListener("storage", freshMainScreen);
-// function checkURL() {
-//   let url = window.location.hash.slice(1) || "/";
-//   if ((url = "/")) {
-//     // document.body.innerHTML = "";
-//     let link = document.createElement("a");
-//     link.href = "#/solutionPage";
-//     link.innerHTML = "go to solution";
-//     document.body.appendChild(link);
-//   }
-//   console.log(url);
-// }
-
-// window.addEventListener("hashchange", checkURL);
-// window.addEventListener("load", checkURL);
-
 /////
-function average() {
-  let count = 0;
-  let number = 0;
-  for (let i = 0; i < 10; i++) {
-    number = Number(prompt(`Enter number: ${i + 1}`));
-    console.log(number);
-    count += number;
-  }
-  const average = count / 10;
-  alert(`everage of numbers is: ${average}`);
-}
-// average();
-function maxGrade() {
-  const grades = [];
-
-  for (let i = 1; i <= 3; i++) {
-    let grade = prompt("Enter a grade:");
-    grades.push(Number(grade));
-  }
-  let highestGrade = Math.max(...grades);
-  console.log(highestGrade);
-}
-
-// maxGrade();
-
-function printNumbers() {
-  let count = Number(prompt("Enter Number: "));
-  for (let i = count; i != 0; i--) {
-    console.log(i);
-  }
-}
-// printNumbers();
-
-function skipSeven() {
-  for (let i = 1; i <= 10; i++) {
-    if (i != 7) {
-      console.log(i);
-    }
-  }
-}
-// skipSeven();
-
-function devideThree() {
-  let count = 0;
-  console.log(count);
-  for (let i = 0; i <= 1000; i++) {
-    if (i % 3 == 0) {
-      count++;
-    }
-  }
-  console.log(count);
-}
-
-// devideThree();
-//
-function guessNumber() {
-  const target = Math.floor(Math.random() * 101);
-  for (let i = 1; i <= 10; i++) {
-    let guess = Number(prompt("Guess the number:"));
-    if (guess == target) {
-      alert("You guessed the right number!");
-      i = 100;
-    } else if (guess < target) {
-      alert("The number you guessed is too low");
-    } else {
-      alert("The number you guessed is too high.");
-    }
-  }
-
-  alert(`The target number was  + ${target}`);
-}
-
-// guessNumber();
